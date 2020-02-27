672ba4508efdc0ba18c217aacd4e8c8b6e5ebc83
sheronova-anastasia <anastasiaaa4278@gmail.com>
2020-02-27 21:03:56 +0400

    lab2klFix

--- TP1/TP1/Bomber.cs
+++ TP1/TP1/Bomber.cs
@@ -7,44 +7,8 @@ using System.Threading.Tasks;
 
 namespace TP1
 {
-    public class Bomber
+    public class Bomber : Plane
     {
-        /// <summary>
-        /// Левая координата отрисовки самолёта
-        /// </summary>
-        private float _startPosX;
-        /// <summary>
-        /// Правая кооридната отрисовки самолёта
-        /// </summary>
-        private float _startPosY;
-        /// <summary>
-        /// Ширина окна отрисовки 
-        /// </summary>
-        private int _pictureWidth;
-        /// <summary>
-        /// Высота окна отрисовки
-        /// </summary>
-        private int _pictureHeight;
-        /// <summary>
-        /// Ширина отрисовки самолёта
-        /// </summary>
-        private const int planeWidth = 100;
-        /// <summary>
-        /// Высота отрисовки самолёта
-        /// </summary>
-        private const int planeHeight = 60;
-        /// <summary>
-        /// Максимальная скорость
-        /// </summary>
-        public int MaxSpeed { private set; get; }
-        /// <summary>
-        /// Вес самолёта
-        /// </summary>
-        public float Weight { private set; get; }
-        /// <summary>
-        /// Основной цвет фюзеляжа
-        /// </summary>
-        public Color MainColor { private set; get; }
         /// <summary>
         /// Дополнительный цвет
         /// </summary>
@@ -72,74 +36,19 @@ namespace TP1
         /// <param name="bomb">Признак наличия бомбы</param>
         /// <param name="rocket">Признак наличия ракеты</param>
         public Bomber(int maxSpeed, float weight, Color mainColor, Color dopColor,
-       bool star, bool bomb, bool rocket)
+       bool star, bool bomb, bool rocket) :
+            base(maxSpeed, weight, mainColor)
         {
-            MaxSpeed = maxSpeed;
-            Weight = weight;
-            MainColor = mainColor;
             DopColor = dopColor;
             Star = star;
             Bomb = bomb;
             Rocket = rocket;
         }
         /// <summary>
-        /// Установка позиции самолёта
-        /// </summary>
-        /// <param name="x">Координата X</param>
-        /// <param name="y">Координата Y</param>
-        /// <param name="width">Ширина картинки</param>
-        /// <param name="height">Высота картинки</param>
-        public void SetPosition(int x, int y, int width, int height)
-        {
-            _startPosX = x;
-            _startPosY = y;
-            _pictureWidth = width;
-            _pictureHeight = height;
-        }
-        /// <summary>
-        /// Изменение направления пермещения
-        /// </summary>
-        /// <param name="direction">Направление</param>
-        public void MoveTransport(Direction direction)
-        {
-            float step = MaxSpeed * 100 / Weight;
-            switch (direction)
-            {
-                // вправо
-                case Direction.Right:
-                    if (_startPosX + step < _pictureWidth - planeWidth)
-                    {
-                        _startPosX += step;
-                    }
-                    break;
-                //влево
-                case Direction.Left:
-                    if (_startPosX - step > 0)
-                    {
-                        _startPosX -= step;
-                    }
-                    break;
-                //вверх
-                case Direction.Up:
-                    if (_startPosY - step > 0)
-                    {
-                        _startPosY -= step;
-                    }
-                    break;
-                //вниз
-                case Direction.Down:
-                    if (_startPosY + step < _pictureHeight - planeHeight)
-                    {
-                        _startPosY += step;
-                    }
-                    break;
-            }
-        }
-        /// <summary>
         /// Отрисовка самолёта
         /// </summary>
         /// <param name="g"></param>
-        public void DrawPlane(Graphics g)
+        public override void DrawPlane(Graphics g)
         {
             Pen pen = new Pen(Color.Black);
             //рисуем звезду
@@ -171,37 +80,7 @@ namespace TP1
                 g.DrawEllipse(pen, _startPosX + 90, _startPosY + 90, 30, 10);
                 g.FillEllipse(bomb, _startPosX + 90, _startPosY + 90, 30, 10);
             }
-            // теперь отрисуем фюзеляж самолета
-            //границы самолета
-            g.DrawRectangle(pen, _startPosX, _startPosY + 40, 130, 20);
-            g.DrawRectangle(pen, _startPosX + 75, _startPosY - 10, 25, 120);
-            Point pointT1 = new Point((int)_startPosX + 5, (int)_startPosY + 20);
-            Point pointT2 = new Point((int)_startPosX + 25, (int)_startPosY + 50);
-            Point pointT3 = new Point((int)_startPosX + 5, (int)_startPosY + 80);
-            Point[] boardT = { pointT1, pointT2, pointT3 };
-            g.DrawPolygon(pen, boardT);
-            Brush brRed = new SolidBrush(MainColor);
-            g.FillRectangle(brRed, _startPosX, _startPosY + 40, 130, 20);
-            Brush brGreen = new SolidBrush(MainColor);
-            g.FillRectangle(brGreen, _startPosX + 75, _startPosY - 10, 25, 120);
-            g.FillPolygon(brGreen, boardT);
-            Brush brBrown = new SolidBrush(MainColor);
-            g.FillEllipse(brBrown, _startPosX + 130, _startPosY + 35, 10, 30);
-            g.DrawEllipse(pen, _startPosX + 130, _startPosY + 35, 10, 30);
-            g.FillEllipse(brBrown, _startPosX + 95, _startPosY, 10, 30);
-            g.DrawEllipse(pen, _startPosX + 95, _startPosY, 10, 30);
-            g.FillEllipse(brBrown, _startPosX + 95, _startPosY + 70, 10, 30);
-            g.DrawEllipse(pen, _startPosX + 95, _startPosY + 70, 10, 30);
-            Brush brBlue = new SolidBrush(MainColor);
-            g.FillEllipse(brBlue, _startPosX + 70, _startPosY + 42, 30, 15);
-            g.DrawEllipse(pen, _startPosX + 70, _startPosY + 42, 30, 15);
-            Brush brPink = new SolidBrush(DopColor);
-            g.FillEllipse(brPink, _startPosX + 133, _startPosY + 55, 5, 17);
-            g.DrawEllipse(pen, _startPosX + 133, _startPosY + 55, 5, 17);
-            g.FillEllipse(brPink, _startPosX + 133, _startPosY + 25, 5, 17);
-            g.DrawEllipse(pen, _startPosX + 133, _startPosY + 25, 5, 17);
-            g.FillEllipse(brPink, _startPosX + 133, _startPosY + 45, 7, 7);
-            g.DrawEllipse(pen, _startPosX + 133, _startPosY + 45, 7, 7);
+            base.DrawPlane(g);
             // рисуем ракеты
             if (Rocket)
             {
---
+++ TP1/TP1/Plane.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    public class Plane : Vehicle
+    {
+        /// <summary>
+        /// Ширина отрисовки самолёта
+        /// </summary>
+        protected const int planeWidth = 100;
+        /// <summary>
+        /// Ширина отрисовки самолёта
+        /// </summary>
+        protected const int planeHeight = 60;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="maxSpeed">Максимальная скорость</param>
+        /// <param name="weight">Вес самолёта</param>
+        /// <param name="mainColor">Основной цвет фюзеляжа</param>
+        public Plane(int maxSpeed, float weight, Color mainColor)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+        }
+        public override void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - planeWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                //влево
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                //вверх
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                //вниз
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - planeHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+        public override void DrawPlane(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            // теперь отрисуем фюзеляж самолета
+            //границы самолета
+            g.DrawRectangle(pen, _startPosX, _startPosY + 40, 130, 20);
+            g.DrawRectangle(pen, _startPosX + 75, _startPosY - 10, 25, 120);
+            Point pointT1 = new Point((int)_startPosX + 5, (int)_startPosY + 20);
+            Point pointT2 = new Point((int)_startPosX + 25, (int)_startPosY + 50);
+            Point pointT3 = new Point((int)_startPosX + 5, (int)_startPosY + 80);
+            Point[] boardT = { pointT1, pointT2, pointT3 };
+            g.DrawPolygon(pen, boardT);
+            Brush brRed = new SolidBrush(MainColor);
+            g.FillRectangle(brRed, _startPosX, _startPosY + 40, 130, 20);
+            Brush brGreen = new SolidBrush(MainColor);
+            g.FillRectangle(brGreen, _startPosX + 75, _startPosY - 10, 25, 120);
+            g.FillPolygon(brGreen, boardT);
+            Brush brBrown = new SolidBrush(MainColor);
+            g.FillEllipse(brBrown, _startPosX + 130, _startPosY + 35, 10, 30);
+            g.DrawEllipse(pen, _startPosX + 130, _startPosY + 35, 10, 30);
+            g.FillEllipse(brBrown, _startPosX + 95, _startPosY, 10, 30);
+            g.DrawEllipse(pen, _startPosX + 95, _startPosY, 10, 30);
+            g.FillEllipse(brBrown, _startPosX + 95, _startPosY + 70, 10, 30);
+            g.DrawEllipse(pen, _startPosX + 95, _startPosY + 70, 10, 30);
+            Brush brBlue = new SolidBrush(MainColor);
+            g.FillEllipse(brBlue, _startPosX + 70, _startPosY + 42, 30, 15);
+            g.DrawEllipse(pen, _startPosX + 70, _startPosY + 42, 30, 15);
+            Brush brPink = new SolidBrush(Color.Brown);
+            g.FillEllipse(brPink, _startPosX + 133, _startPosY + 55, 5, 17);
+            g.DrawEllipse(pen, _startPosX + 133, _startPosY + 55, 5, 17);
+            g.FillEllipse(brPink, _startPosX + 133, _startPosY + 25, 5, 17);
+            g.DrawEllipse(pen, _startPosX + 133, _startPosY + 25, 5, 17);
+            g.FillEllipse(brPink, _startPosX + 133, _startPosY + 45, 7, 7);
+            g.DrawEllipse(pen, _startPosX + 133, _startPosY + 45, 7, 7);
+        }
+    }
+}
