63615a5b12c2b013a94b67a316a3abfa75e14ca8
sheronova-anastasia <anastasiaaa4278@gmail.com>
2020-03-11 19:32:15 +0400

    !!

--- TP1/TP1/FormHangar.Designer.cs
+++ TP1/TP1/FormHangar.Designer.cs
@@ -34,6 +34,15 @@
             this.buttonGet = new System.Windows.Forms.Button();
             this.maskedTextBoxPlace = new System.Windows.Forms.MaskedTextBox();
             this.labelPlace = new System.Windows.Forms.Label();
+            this.listBoxLevels = new System.Windows.Forms.ListBox();
+            this.labelLevels = new System.Windows.Forms.Label();
+            this.buttonAddPlane = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialogHangar = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialogHangar = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHangar)).BeginInit();
             this.groupBoxTakePlane.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakePlane)).BeginInit();
@@ -54,6 +63,9 @@
             this.groupBoxTakePlane.Controls.Add(this.buttonGet);
             this.groupBoxTakePlane.Controls.Add(this.maskedTextBoxPlace);
             this.groupBoxTakePlane.Controls.Add(this.labelPlace);
+            this.groupBoxTakePlane.Location = new System.Drawing.Point(802, 241);
+            this.groupBoxTakePlane.Name = "groupBoxTakePlane";
+            this.groupBoxTakePlane.Size = new System.Drawing.Size(170, 227);
             this.groupBoxTakePlane.TabIndex = 3;
             this.groupBoxTakePlane.TabStop = false;
             this.groupBoxTakePlane.Text = "Забрать самолёт";
@@ -62,7 +74,7 @@
             // 
             this.pictureBoxTakePlane.Location = new System.Drawing.Point(6, 93);
             this.pictureBoxTakePlane.Name = "pictureBoxTakePlane";
-            this.pictureBoxTakePlane.Size = new System.Drawing.Size(158, 109);
+            this.pictureBoxTakePlane.Size = new System.Drawing.Size(158, 128);
             this.pictureBoxTakePlane.TabIndex = 4;
             this.pictureBoxTakePlane.TabStop = false;
             // 
@@ -91,11 +103,86 @@
             this.labelPlace.Size = new System.Drawing.Size(42, 13);
             this.labelPlace.TabIndex = 1;
             this.labelPlace.Text = "Место:";
-          
+            // 
+            // listBoxLevels
+            // 
+            this.listBoxLevels.FormattingEnabled = true;
+            this.listBoxLevels.Location = new System.Drawing.Point(830, 26);
+            this.listBoxLevels.Name = "listBoxLevels";
+            this.listBoxLevels.Size = new System.Drawing.Size(120, 95);
+            this.listBoxLevels.TabIndex = 4;
+            this.listBoxLevels.SelectedIndexChanged += new System.EventHandler(this.listBoxLevels_SelectedIndexChanged);
+            // 
+            // labelLevels
+            // 
+            this.labelLevels.AutoSize = true;
+            this.labelLevels.Location = new System.Drawing.Point(860, 10);
+            this.labelLevels.Name = "labelLevels";
+            this.labelLevels.Size = new System.Drawing.Size(48, 13);
+            this.labelLevels.TabIndex = 5;
+            this.labelLevels.Text = "Уровни:";
+            // 
+            // buttonAddPlane
+            // 
+            this.buttonAddPlane.Location = new System.Drawing.Point(830, 165);
+            this.buttonAddPlane.Name = "buttonAddPlane";
+            this.buttonAddPlane.Size = new System.Drawing.Size(120, 55);
+            this.buttonAddPlane.TabIndex = 6;
+            this.buttonAddPlane.Text = "Добавить самолёт";
+            this.buttonAddPlane.UseVisualStyleBackColor = true;
+            this.buttonAddPlane.Click += new System.EventHandler(this.buttonAddPlane_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip.TabIndex = 7;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // saveFileDialogHangar
+            // 
+            this.saveFileDialogHangar.Filter = "txt file | *.txt";
+            // 
+            // openFileDialogHangar
+            // 
+            this.openFileDialogHangar.FileName = "openFileDialog1";
+            this.openFileDialogHangar.Filter = "file text | *.txt";
+            // 
             // FormHangar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 465);
+            this.Controls.Add(this.buttonAddPlane);
+            this.Controls.Add(this.labelLevels);
+            this.Controls.Add(this.listBoxLevels);
+            this.Controls.Add(this.groupBoxTakePlane);
             this.Controls.Add(this.pictureBoxHangar);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
@@ -120,6 +207,15 @@
         private System.Windows.Forms.Button buttonGet;
         private System.Windows.Forms.MaskedTextBox maskedTextBoxPlace;
         private System.Windows.Forms.Label labelPlace;
+        private System.Windows.Forms.ListBox listBoxLevels;
+        private System.Windows.Forms.Label labelLevels;
+        private System.Windows.Forms.Button buttonAddPlane;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogHangar;
+        private System.Windows.Forms.OpenFileDialog openFileDialogHangar;
 
     }
 }
